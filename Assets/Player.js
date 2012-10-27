f73551262aa006f04198356d0709661fc5d7b0ee
Robert Dionne <robertsdionne@gmail.com>
2012-10-26 21:05:14 -0400

    Add an intelligent camera.

--- Assets/Player.js
+++ Assets/Player.js
@@ -1,5 +1,9 @@
 #pragma strict
 
+private static var XZ : Vector3 = Vector3(1, 0, 1);
+
+var maxCameraDistance : float;
+var mainCamera : Transform;
 var speed : float; // m/s
 
 private var controller : CharacterController;
@@ -9,8 +13,16 @@ function Start () {
 }
 
 function Update () {
-  var dr : Vector3 = Input.GetAxis("Horizontal") * transform.right;
-  var df : Vector3 = Input.GetAxis("Vertical") * transform.forward;
+  mainCamera.transform.LookAt(transform);
+  var directionToPlayer : Vector3 = 
+      Vector3.Scale(XZ, transform.position) - Vector3.Scale(XZ, mainCamera.position);
+  var cameraDistance : float = directionToPlayer.magnitude;
+  if (cameraDistance > maxCameraDistance) {
+    var correction : Vector3 = directionToPlayer.normalized * (cameraDistance - maxCameraDistance);
+    mainCamera.transform.position += correction;
+  }
+  var dr : Vector3 = Input.GetAxis("Horizontal") * mainCamera.right;
+  var df : Vector3 = Input.GetAxis("Vertical") * mainCamera.forward;
   var velocity : Vector3 = speed * Vector3.ClampMagnitude(dr + df, 1);
   controller.SimpleMove(velocity);
 }
