4e3138d60117c1d9b735e1dd0de82ac547c17d9c
Robert Dionne <robertsdionne@gmail.com>
2012-10-26 20:32:24 -0400

    Add some dumb player movement.

--- Assets/Player.js
+++ Assets/Player.js
@@ -1,9 +1,14 @@
 #pragma strict
 
+var speed : float; // m/s
+
 function Start () {
 
 }
 
 function Update () {
-
+  var dr : Vector3 = Input.GetAxis("Horizontal") * transform.right;
+  var df : Vector3 = Input.GetAxis("Vertical") * transform.forward;
+  var velocity : Vector3 = speed * Time.deltaTime * Vector3.ClampMagnitude(dr + df, 1);
+  transform.position += velocity;
 }
