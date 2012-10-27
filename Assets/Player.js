266210d982b3187bbfbe8fd18bdde8d800d24846
Robert Dionne <robertsdionne@gmail.com>
2012-10-26 20:43:05 -0400

    Switch to use a CharacterController for movement to enable collisions.

--- Assets/Player.js
+++ Assets/Player.js
@@ -2,13 +2,15 @@
 
 var speed : float; // m/s
 
-function Start () {
+private var controller : CharacterController;
 
+function Start () {
+  controller = GetComponent(CharacterController);
 }
 
 function Update () {
   var dr : Vector3 = Input.GetAxis("Horizontal") * transform.right;
   var df : Vector3 = Input.GetAxis("Vertical") * transform.forward;
-  var velocity : Vector3 = speed * Time.deltaTime * Vector3.ClampMagnitude(dr + df, 1);
-  transform.position += velocity;
+  var velocity : Vector3 = speed * Vector3.ClampMagnitude(dr + df, 1);
+  controller.SimpleMove(velocity);
 }
