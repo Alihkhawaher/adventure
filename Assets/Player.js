16938a20a9a4c3d8be3e485e4a0947f9d2c1e1b6
Robert Dionne <robertsdionne@gmail.com>
2012-10-26 22:12:01 -0400

    Add a minimum camera distance. Add camera nudging with right gamepad stick.

--- Assets/Player.js
+++ Assets/Player.js
@@ -2,6 +2,8 @@
 
 private static var XZ : Vector3 = Vector3(1, 0, 1);
 
+var angle : float;
+var minCameraDistance : float;
 var maxCameraDistance : float;
 var mainCamera : Transform;
 var speed : float; // m/s
@@ -12,17 +14,25 @@ function Start () {
   controller = GetComponent(CharacterController);
 }
 
-function Update () {
+function FixedUpdate () {
   mainCamera.transform.LookAt(transform);
   var directionToPlayer : Vector3 = 
       Vector3.Scale(XZ, transform.position) - Vector3.Scale(XZ, mainCamera.position);
   var cameraDistance : float = directionToPlayer.magnitude;
+  var correction : Vector3;
   if (cameraDistance > maxCameraDistance) {
-    var correction : Vector3 = directionToPlayer.normalized * (cameraDistance - maxCameraDistance);
+    correction = directionToPlayer.normalized * (cameraDistance - maxCameraDistance);
+    mainCamera.transform.position += correction;
+  } else if (cameraDistance < minCameraDistance) {
+    correction = directionToPlayer.normalized * (cameraDistance - minCameraDistance);
     mainCamera.transform.position += correction;
   }
   var dr : Vector3 = Input.GetAxis("Horizontal") * mainCamera.right;
-  var df : Vector3 = Input.GetAxis("Vertical") * mainCamera.forward;
+  var df : Vector3 = Input.GetAxis("Vertical") * directionToPlayer;
+  var rotation : Quaternion =
+      Quaternion.AngleAxis(Input.GetAxis("VerticalHat") * angle, mainCamera.right) *
+          Quaternion.AngleAxis(Input.GetAxis("HorizontalHat") * angle * 1.5, mainCamera.up);
+  mainCamera.rotation = rotation * mainCamera.rotation;
   var velocity : Vector3 = speed * Vector3.ClampMagnitude(dr + df, 1);
   controller.SimpleMove(velocity);
 }
