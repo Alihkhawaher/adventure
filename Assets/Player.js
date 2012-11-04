967ae0503aaa36d3edd1c2d36513e492a0661e1e
Ilya Zarembsky <ilya.zarembsky@gmail.com>
2012-11-04 01:47:47 -0400

    IVZ test
    Added test comment to Player script

--- Assets/Player.js
+++ Assets/Player.js
@@ -81,3 +81,5 @@ function Update () {
       Mathf.Clamp(Input.GetAxis(ZOOM), -1, 0) * zoomedFieldOfView +
       Mathf.Clamp(Input.GetAxis(ZOOM), 0, 1) * unzoomedFieldOfView, cameraAlpha);
 }
+
+#IVZ TEST
